4a785b382af95cebe262be14db3f347b9cbe749a
csterbenz1 <cster@g.ucla.edu>
2019-11-07 23:32:25 -0800

    adding k/spacing

--- R/functions.R
+++ R/functions.R
@@ -3,6 +3,8 @@
 ### Build kernel, possibly non-square
 makeK = function(allx, useasbases=NULL, b=NULL){
   N=nrow(allx)
+  # If no "useasbasis" given, assume all observations are to be used.
+  #if (is.null(b)){ b=2*ncol(allx) }
   bases = allx[useasbases==1, ]
   K=KRLS2::newKernel(X = bases , newData = allx , b = b)
 }
@@ -23,14 +25,14 @@ makeK = function(allx, useasbases=NULL, b=NULL){
 biasbound=function(observed, target, svd.out, w, hilbertnorm=1){
   wtarget=w[target==1]/sum(target==1)
   wobserved=w[observed==1]/sum(observed==1)
-  
+
   V=svd.out$u
   eigenvals=svd.out$d
-  
+
   V1=V[target==1, , drop=FALSE]
   V0=V[observed==1, , drop=FALSE]
   eigenimbal=as.vector(t(wtarget)%*%V1 - t(wobserved)%*%V0)
-  
+
   effectiveimbal=(eigenimbal*(eigenvals^.5)) #%*%t(V)
   biasbound=sqrt(hilbertnorm)*sqrt(t(effectiveimbal)%*%(effectiveimbal))
   return(biasbound)
@@ -40,29 +42,33 @@ biasbound=function(observed, target, svd.out, w, hilbertnorm=1){
 # (Not actually used right now but can be convenient)
 
 #' Difference in means and Difference in weighted means
-#' 
+#'
 #' Calcuates the simple difference in means or weighted difference in means between the treated/sample population and the control/target popultion.
 #' 
 #' @param X Matrix of data where rows are observations and columns are covariates.
 #' @param w numeric vector of weights for each observation.
 #' @param target a numeric vector of length equal to the total number of units where population units take a value of 1 and sample units take a value of 0. 
+#'
+#' @param X Matrix of combined treated/sample population and control/target population data. Rows are observations, columns are covariates.
+#' @param w vector of weights
+#' @param target vector taking values of 0 or 1's indicating which observations (whuch rows  of X and w) are in the treated/sample population and which are in the control/target population.
 #' @return \code{dim} the simple, unweighted difference in means
 #' @return \code{dimw} the weighted difference in means
-#' @example XXX 
+#' @example XXX
 dimw = function(X,w,target){
   w1=w[target==1]/sum(w[target==1])
   w0=w[target!=1]/sum(w[target!=1])
-  
+
   X1=X[target==1, , drop=FALSE]
   X0=X[target!=1, , drop=FALSE]
-  
+
   R=list()
   R$dim=colMeans(X1)-colMeans(X0)
   R$dimw=t(as.vector(w1))%*%X1-t(w0)%*%X0
 }
 
 # Function to get the moments that solve the constraints
-# we have setup. 
+# we have setup.
 # Currently just uses ebal, but we could to others
 # or give options.
 # Will need some work to better carry error messages from ebal.
@@ -78,14 +84,14 @@ dimw = function(X,w,target){
 #' @example XXX
 #' @export
 getw = function(target, observed, allrows, ebal.tol=1e-6,...){
-  
-  # To trick ebal into using a control group that corresponds to the 
-  # observed and a treated that corresponds to the "target" group, 
+
+  # To trick ebal into using a control group that corresponds to the
+  # observed and a treated that corresponds to the "target" group,
   # (1) anybody who is "observed" but also considered part of the target
-  # group has to get a duplicate row in the data 
+  # group has to get a duplicate row in the data
   # (2) construct a variables target_ebal that = 1 when target=1
-  # but =0 in the appended data, i.e. for the observed who are 
-  # entering a second time. 
+  # but =0 in the appended data, i.e. for the observed who are
+  # entering a second time.
   Xappended = rbind(allrows,  allrows[observed==1 & target==1, , drop=FALSE] )
   target_ebal = c(target, rep(0, sum(observed==1 & target==1)))
 
@@ -98,7 +104,7 @@ getw = function(target, observed, allrows, ebal.tol=1e-6,...){
     w=rep(1,N)
     #R$dist="ebalerror"
   }
-  
+
   if (class(bal.out.pc)[1]!="try-error"){
     w=rep(1,N)
     w[observed==1]=bal.out.pc$w
@@ -113,9 +119,9 @@ getw = function(target, observed, allrows, ebal.tol=1e-6,...){
 } # end of getw.
 
 
-# Oct 16 2019 Update on reasoning of names; two forms of input to kpop and 
+# Oct 16 2019 Update on reasoning of names; two forms of input to kpop and
 #a third language change to the whole code
-# 1. pass in "sampled" and a logical "sampledinpop" that when true indicates 
+# 1. pass in "sampled" and a logical "sampledinpop" that when true indicates
 #that the sampled pop should be re-incorperated into the target population
 # 2. pass in "treatment" only which specifies the target population;
 #zeros in this matrix are inferred to be the "sampled" or observed popultion
@@ -130,18 +136,16 @@ getw = function(target, observed, allrows, ebal.tol=1e-6,...){
 # your intended target population should re-include units who happened to also be sampled.  Throw an error is sampleinpop is not specified (i.e. no default).
 # There will no longer be a "target" vector at all.
 
-
-
 # OLD stuff (pre 10 Oct 2019)
 # Added "sampled", in addition to "target".
-# If you tell it only who was sampled, it assumes the targets are 
-# the non-sampled. 
+# If you tell it only who was sampled, it assumes the targets are
+# the non-sampled.
 # If you don't tell it who was sampled but do tell it who the targets are,
 # it assumes the sampled are the non-targets.
 # If you specify both, you can use sampled to tell it whose data you have
-# and use target to specify who should inform the target group, without restriction. 
+# and use target to specify who should inform the target group, without restriction.
 # But the usual use case for specifying both would be to say everybody
-# is in the target, and only the sampled are sampled (which also the default 
+# is in the target, and only the sampled are sampled (which also the default
 # if you say nothing about the target.)
 
 
@@ -167,9 +171,9 @@ getw = function(target, observed, allrows, ebal.tol=1e-6,...){
 #'  \item{dist.sequence}{A numeric vector recording the number of dimensions of the SVD of the kernel matrix used in calculating the bias bound values stored in \code{dist.record}}
 #'  \item{biasbound.orig}{The bias bound found when all sampled units have a weight of one over the number of sampled units and all target units have a weight of one over the number of target units.XX}
 #'  \item{numdims}{The optimal number of dimensions of the SVD of the kernel matrix which minimizes the bias bound.}
-#'  #'  \item{w}{The weights found using entropy balancing on \code{numdims} dimensions of the SVD of the kernel matrix}
+#'  \item{w}{The weights found using entropy balancing on \code{numdims} dimensions of the SVD of the kernel matrix}
 #'  \item{biasbound.opt}{The minimal bias bound found using \code{numdims} as the number of dimestions of the SVD of the kernel matrix. When \code{numdims} is user-specified, the bias bound using this number of dimensions of the kernel matrix.}
-#' 
+#' \item{K}{The kernel matrix.}
 #' @example 
 #' library(KBAL)
 #' #Run Lalonde example as in paper:
@@ -206,10 +210,10 @@ kpop = function(allx, useasbases=NULL, b=NULL,
       warning("using default parameter \"sampledinpop\" = TRUE")
       sampledinpop = TRUE
   }
-    
+
   N=nrow(allx)
   user_numdims = NULL
-  if(!is.null(sampled) & sampledinpop==FALSE) { 
+  if(!is.null(sampled) & sampledinpop==FALSE) {
       observed = sampled
       target = 1-sampled
   } else if(!is.null(sampled)) {
@@ -242,9 +246,18 @@ kpop = function(allx, useasbases=NULL, b=NULL,
       warning("Dimensions of K greater than 2000, using sampled as default bases")
       useasbases = as.numeric(observed==1)
   }
- 
+
+  # If we don't specify which observations to use as bases, 
+  # use all as default unless K is very large, then use sample set. 
+  if (is.null(useasbases) & N <= 2000){
+      useasbases = rep(1,N) 
+  } else if(is.null(useasbases)) {
+      warning("Dimensions of K greater than 2000, using sampled as default bases")
+      useasbases = as.numeric(observed==1)
+  }
+
   if (is.null(minnumdims)){minnumdims=1}
-  
+
   # The most dims you can use is the number of bases
   if (!is.null(maxnumdims) && maxnumdims>sum(useasbases)){
     warning("Cannot allow dimensions of K to be greater than the number of bases. Reducing maxnumdims.")
@@ -259,12 +272,12 @@ kpop = function(allx, useasbases=NULL, b=NULL,
   K = makeK(allx = allx, useasbases = useasbases, b=b)
   svd.out=svd(K)
   Kpc=svd.out$u
-  
+
   # Get biasbound with no improvement in balance:
   biasbound_orig=biasbound( w = rep(1,N), observed=observed, target = target, 
                             svd.out = svd.out, hilbertnorm = 1)
   paste0("Without balancing, biasbound (norm=1) is ",round(biasbound_orig,3))
-  
+
   # If numdims given, just get the weights in one shot:
   if (!is.null(numdims)){
     Kpc2=Kpc[,1:numdims, drop=FALSE]
@@ -277,7 +290,7 @@ kpop = function(allx, useasbases=NULL, b=NULL,
                  round(biasboundnow,3)))
     user_numdims = TRUE
     }
-  
+
   # If numdims not given, we search to minimize biasbound:
   if (is.null(numdims)){
     thisnumdims=minnumdims
@@ -291,9 +304,9 @@ kpop = function(allx, useasbases=NULL, b=NULL,
       Kpc_try=Kpc[,1:thisnumdims, drop=FALSE]
       getw.out=getw(target = target, observed=observed, allrows = Kpc_try)
       w=getw.out$w
-      # Need to work on case where ebal fails and flagging this in result. 
-      # For now just returns all even weights. 
-      
+      # Need to work on case where ebal fails and flagging this in result.
+      # For now just returns all even weights.
+
       biasboundnow=biasbound( w = w, observed=observed, target = target, svd.out = svd.out, hilbertnorm = 1)
       if(printprogress == TRUE) {
           print(paste0("With ",thisnumdims," dimensions, biasbound (norm=1) of ", 
@@ -301,33 +314,33 @@ kpop = function(allx, useasbases=NULL, b=NULL,
       }
      
       dist.record=c(dist.record,biasboundnow)
-      
+
       dist.now=biasboundnow # To make more generic, distance could be any measure.
       dist.orig=biasbound_orig
 
       thisnumdims=thisnumdims+incrementby
-      
+
       if (dist.now<mindistsofar){mindistsofar=dist.now}
-      
+
       wayover=(dist.now/mindistsofar)>1.25
-      
-      keepgoing= (thisnumdims<=maxnumdims) & (wayover==FALSE) #& (dist.now<dist.orig) 
+
+      keepgoing= (thisnumdims<=maxnumdims) & (wayover==FALSE) #& (dist.now<dist.orig)
       #& dist.now!="error"
       # (dist.now>mindistsofar)  # XXX this was in there, but needed?
-      # XXX In above, need to work on "keepgoing" for case where ebal 
-      # is failing. 
+      # XXX In above, need to work on "keepgoing" for case where ebal
+      # is failing.
     } # End of while loop for "keepgoing"
-    
+
     dimseq=seq(minnumdims,maxnumdims,incrementby)
     numdims=dimseq[which(dist.record==min(dist.record,na.rm=TRUE))]
-    
+
     #if nothing improved balance, there will be "many" minima.
     # throw warning, and choose the fewest numdims.
     if (length(numdims)>1){
       warning("Lack of improvement in balance; choosing fewest dimensions to balance on among those with the same (lack of) improvement. But beware that balance is likely poor.")
       numdims=min(numdims)
     }
-    
+
     # Finally, we didn't save weights each time, so go back and re-run
     # at optimal  number of dimensions
     if(is.null(user_numdims)){
@@ -338,8 +351,8 @@ kpop = function(allx, useasbases=NULL, b=NULL,
     w=getw.out$w
     biasbound_opt=biasbound( w = w, observed=observed, target = target, svd.out = svd.out, hilbertnorm = 1)
 }   #end for "if null numdims"
-  
-  
+
+
   R=list()
   R$dist.record=dist.record
   R$dist.sequence=dimseq[1:length(dist.record)]
@@ -347,6 +360,7 @@ kpop = function(allx, useasbases=NULL, b=NULL,
   R$w=w
   R$numdims=numdims
   R$biasbound.opt=biasbound_opt
+  R$K = K
 
   return(R)
 } # end kpop main function
